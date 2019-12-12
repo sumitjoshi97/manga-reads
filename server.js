8e355450f72bca2f7b1d82c94232416ad27c12a2
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-12-12 12:48:15 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 
+const mongoose = require('mongoose')
+
+const schema = require('./graphql/schema')
+const keys = require('./keys')
+
+mongoose.connect(keys.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+require('./cron')
+
 const app = express()
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: MyGraphQLSchema,
+    schema,
     graphiql: true,
   })
 )
@@ -15,4 +27,4 @@ app.get('/', (req, res) => res.send('server running'))
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`server running on port ${PORT}`))
+app.listen(port, () => console.log(`server running on port ${port}`))
