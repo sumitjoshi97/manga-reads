836cd167b822b0cce15c018f56a4ebece7476d9c
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-12-19 11:35:31 +0530

    add chapter type and manga query

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,15 +1,31 @@
-const typeDefs = `
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
   scalar Date
+
+  type Chapter {
+    id: ID!
+    lastUpdated: Date!
+    number: String!
+    title: String!
+  }
 
   type Manga {
     id: ID!
     image: String
     lastUpdated: Date!
     title: String!
+    info: MangaInfo!
+  }
+
+  type MangaInfo {
+    id: ID!
+    chapters: [Chapter!]!
   }
 
   type Query {
     mangas: [Manga!]!
+    manga(id: ID!): Manga!
   }
 `
 
