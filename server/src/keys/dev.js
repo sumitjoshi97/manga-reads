503f8039e4a5a3921e4075aeabb94751424f1a7f
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-12-30 13:25:24 +0530

    removed files

--- server/src/keys/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DB_URL:
-    'mongodb+srv://sj:sumit123@cluster0-jylcd.mongodb.net/test?retryWrites=true&w=majority',
-  MANGA_EDEN_URL: 'https://www.mangaeden.com/api/list/0/',
-}
