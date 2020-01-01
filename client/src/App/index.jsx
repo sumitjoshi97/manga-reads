b4550c022288a775f3267ea36ef6ffa8fd91a26d
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-01-01 17:29:43 +0530

    added root App component

---
+++ client/src/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from '../components/Search'
+
+export default function App() {
+  return (
+    <div className="main-container">
+      <div className="main-container__search-container">
+        <Search />
+      </div>
+    </div>
+  )
+}
